b7b95d890f1932f72053d060d89a20864f5a416d
Lorenzo <lorenzo.francioli@mail.polimi.it>
2020-05-27 10:41:33 +0200

    master: interrupt on pin 3.2 to enable/disable configuration but the EEPROM writing is not present. it works in every condition (even starting with 3.2 connected to GND)

--- Final_Assignment/Final_assignment_01.cydsn/codegentemp/Final_assignment_01.v
+++ Final_Assignment/Final_assignment_01.cydsn/codegentemp/Final_assignment_01.v
@@ -1,6 +1,6 @@
 // ======================================================================
 // Final_assignment_01.v generated from TopDesign.cysch
-// 05/26/2020 at 15:29
+// 05/27/2020 at 10:25
 // This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 // ======================================================================
 
